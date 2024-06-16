e86b8382fdcb593446d7a66c208d3a7488e106c9
Luca <lucatonello097@gmail.com>
2024-06-16 18:54:38 -0300

    board and ship factories almost done

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function ship(length) {
     let hitCount = 0;
     return {
         length: length,
+        //position: position,
         hit: function() {
             hitCount ++;
             return hitCount
@@ -21,14 +22,37 @@ function gameboard() {
     const board = Array(10).fill(null).map(() => Array(10).fill(null));
     return {
         board: board,
-        recieveAttack: function(cords) {
-            let [x, y] = cords;
+        placeShip: function(start, length, orientation) {
+            let [x, y] = start;
+            const newShip = ship(length);
+            
+            if (orientation === 'horizontal') {
+                if (y + length > 10) throw new Error('Ship out of bounds');
+                for (let i = 0; i < length; i++) {
+                    if (board[x][y + i] !== null) throw new Error('overlaping');
+                    board[x][y + i].newShip;
+                }
+            }
+            else if (orientation === 'vertical') {
+                if (x + length > 10) throw new Error('Ship out of bounds');
+                for (let i = 0; i < length; i++) {
+                    if (board[x + i][y] !== null) throw new Error('overlaping');
+                    board[x + i][y].newShip;
+                }
+            }
+            else {
+                throw new Error('Invalid orientation')
+            }
+        },
+        recieveAttack: function(attackCords) {
+            let [x, y] = attackCords;
             if (x >= 0 && x <= 10 && y <= 0 && y <= 10) {
                 if (board[x, y] === null) {
                     board[x, y] = 'miss';
                 }
                 else if (board[x, y] === Object) {
                     board[x, y].hit();
+                    isSunk();
                 }
             }
             else {
